48089cfd12e13df22ea573aa5ee493ab79e00b3f
abbasghori29 <132843772+abbasghori29@users.noreply.github.com>
2024-08-01 15:31:00 +0500

    Initial commit

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -4,7 +4,7 @@ import Media from './Media';
 
 const LeftBanner = () => {
   const [text] = useTypewriter({
-    words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
+    words: ["ARTIFICIAL INTELLIGENCE / MACHINE LEARNING ENGINEER.", "DATA SCIENTIST / ANALYST", , "Full Stack Developer.", " PYTHON AUTOMATION DEVELOPER"],
     loop: true,
     typeSpeed: 20,
     deleteSpeed: 10,
